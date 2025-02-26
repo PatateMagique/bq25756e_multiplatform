8e84fff97dc211d58049226fb2c7f84cac4b354b
PatateMagique <theo.heng@epfl.ch>
2025-02-26 18:00:16 +0100

    Finished repo initialization

--- include/Platform.h
+++ include/Platform.h
@@ -1,4 +1,11 @@
-// Platform.h
+/*
+ * Copyright (c) 2025 Théo Heng
+ *
+ * This file is part of the bq25756e_multiplatform library.
+ *
+ * Licensed under the MIT License. See the LICENSE file in the project root for full license information.
+ */
+
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
--- src/I2C.cpp
+++ src/I2C.cpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2025 Théo Heng
+ *
+ * This file is part of the bq25756e_multiplatform library.
+ *
+ * Licensed under the MIT License. See the LICENSE file in the project root for full license information.
+ */
+
 #include "I2C.h"
 
 // Write a value to a specific DRV8214 register for a given driver address
